1e083b604890513de3295376310dd4f7b7fb4d7f
Filbert-code <alexlingfungfilbert@gmail.com>
2022-10-29 14:06:02 -0700

    Added save and load feature

--- main_frame.py
+++ main_frame.py
@@ -1,7 +1,13 @@
+import os
 import tkinter as tk
 import tkinter.ttk as ttk
+from datetime import datetime
 from tkinter import N, S, E, W
 from tasks import Tasks
+from tkcalendar import Calendar
+
+PATH = os.getcwd()
+TASK_DATA_DIR = 'task_data'
 
 
 class MainFrame(ttk.Frame):
@@ -10,31 +16,85 @@ class MainFrame(ttk.Frame):
         self.root = root
         self.root.option_add('*tearOff', False)
         self.root.title('Tasks Scheduler')
-        self.root.rowconfigure(0, weight=1)
-        self.root.columnconfigure(0, weight=1)
+        # self.root.rowconfigure(0, weight=1, minsize=300)
+        # self.root.columnconfigure(0, weight=1)
+        self.history = self.load_history()
 
         # root frame setup
         self.root_frame = ttk.Frame(self.root)
         self.root_frame.grid(row=0, column=0, sticky=N + S + E + W)
-        self.root_frame.grid_rowconfigure(1, weight=1)
-        self.root_frame.grid_columnconfigure(1, weight=1)
+        self.root_frame.grid_rowconfigure(0, weight=1, minsize=300)
+        self.root_frame.grid_columnconfigure(0, weight=1)
 
+        # menu setup
+        self.menu_bar = tk.Menu(self.root_frame)
+        self.menu_file = tk.Menu(self.menu_bar)
+        self.root['menu'] = self.menu_bar
+        self.menu_bar.add_cascade(menu=self.menu_file, label='File')
+        self.menu_file.add_command(label='Save', command=self.save)
+        self.menu_file.add_command(label='Load')
+
+        # tasks notebook setup
         self.morning_tab = ttk.Notebook(self.root_frame)
         self.morning_tab.grid(row=0, column=0, sticky=N + S + E + W)
-        self.morning_tab.grid_rowconfigure(1, weight=1)
-        self.morning_tab.grid_columnconfigure(1, weight=1)
 
-        self.all_tasks = Tasks(self.morning_tab, task_category_name='ALL')
-        self.early_morning_tasks = Tasks(self.morning_tab, task_category_name='Early Morning')
-        self.late_morning_tasks = Tasks(self.morning_tab, task_category_name='Late Morning')
-        self.early_afternoon_tasks = Tasks(self.morning_tab, task_category_name='Early Afternoon')
-        self.late_afternoon_tasks = Tasks(self.morning_tab, task_category_name='Late Afternoon')
+        self.early_morning_tasks = Tasks(self.morning_tab, task_category_name='Early Morning', task_entries=self.history['Early Morning'])
+        self.late_morning_tasks = Tasks(self.morning_tab, task_category_name='Late Morning', task_entries=self.history['Late Morning'])
+        self.early_afternoon_tasks = Tasks(self.morning_tab, task_category_name='Early Afternoon', task_entries=self.history['Early Afternoon'])
+        self.late_afternoon_tasks = Tasks(self.morning_tab, task_category_name='Late Afternoon', task_entries=self.history['Late Afternoon'])
 
-        self.morning_tab.add(self.all_tasks.frame, text='ALL')
+        self.task_data = [
+            (self.early_morning_tasks.task_entries, 'Early Morning'),
+            (self.late_morning_tasks.task_entries, 'Late Morning'),
+            (self.early_afternoon_tasks.task_entries, 'Early Afternoon'),
+            (self.late_afternoon_tasks.task_entries, 'Late Afternoon'),
+        ]
+
         self.morning_tab.add(self.early_morning_tasks.frame, text='Early Morning')
         self.morning_tab.add(self.late_morning_tasks.frame, text='Late Morning')
         self.morning_tab.add(self.early_afternoon_tasks.frame, text='Early Afternoon')
         self.morning_tab.add(self.late_afternoon_tasks.frame, text='Late Afternoon')
+
+        # set the list for all tasks
+
+    def load_history(self):
+        todays_date = datetime.now().strftime("%m.%d.%Y")
+        tasks_dir = os.path.join(PATH, TASK_DATA_DIR)
+        tasks_data_filenames = os.listdir(tasks_dir)
+        for tasks_filename in tasks_data_filenames:
+            if tasks_filename == todays_date + '.txt':
+                # load the file data
+                with open(os.path.join(tasks_dir, todays_date + '.txt'), 'r') as file_reader:
+                    lines = file_reader.read().splitlines()
+
+        data = {'Early Morning': [], 'Late Morning': [], 'Early Afternoon': [], 'Late Afternoon': []}
+        for line in lines:
+            state, text, task_category = line.split(', ')
+            for key, value in data.items():
+                if task_category == key:
+                    data[key].append({
+                        'State': state,
+                        'Text': text,
+                    })
+        return data
+
+    def save(self):
+        todays_date = datetime.now().strftime("%m.%d.%Y")
+        tasks_dir = os.path.join(PATH, TASK_DATA_DIR)
+
+        lines = []
+        for data, task_category in self.task_data:
+            lines += self.convert_task_data_to_save_file_format(data, task_category)
+
+        with open(os.path.join(tasks_dir, todays_date + '.txt'), 'w') as file_writer:
+            file_writer.write('\n'.join(lines))
+
+    def convert_task_data_to_save_file_format(self, task_data, task_category):
+        return [f'{", ".join([task["State"].get(), task["Text"].get(), task_category])}' for task in task_data]
+
+    def load(self):
+        pass
+
 
     #     self.inf_loop()
     #
--- tasks.py
+++ tasks.py
@@ -6,13 +6,12 @@ import custom_fonts
 
 
 class Tasks(ttk.Frame):
-    def __init__(self, parent, task_category_name):
+    def __init__(self, parent, task_category_name, task_entries=None):
         ttk.Frame.__init__(self, parent)
         self.parent = parent
 
         self.frame = ttk.Frame(self.parent)
 
-        self.task_entries = []
         self.next_task_row_num = 2
 
         # tasks
@@ -20,29 +19,33 @@ class Tasks(ttk.Frame):
         self.add_more_button = ttk.Button(self.frame, text='ADD', command=self.add_new_task, font=custom_fonts.DEFAULT)
         self.add_more_button.grid(row=1, column=0)
 
-        # create first empty task
-        self.add_new_task()
+        self.task_entries = []
+        if not task_entries:
+            self.add_new_task()
+        else:
+            self.set_tasks(task_entries)
 
-    def add_new_task(self):
+    def add_new_task(self, checkbox_value='0', text_value=''):
         task_frame = ttk.Frame(self.frame)
         task_frame.grid(row=self.next_task_row_num, column=0)
         self.next_task_row_num += 1
 
         # create entry box
-        text = tk.StringVar()
+        text = tk.StringVar(value=text_value)
         entry = ttk.Entry(task_frame, textvariable=text, font=custom_fonts.DEFAULT, width=40)
         entry.grid(row=0, column=1)
 
         def change_entry_state():
-            if entry['state'] == NORMAL:
-                entry['state'] = DISABLED
-            else:
+            if check_flag.get() == '0':
                 entry['state'] = NORMAL
+            else:
+                entry['state'] = DISABLED
 
         # completed task checkmark
-        check_flag = tk.StringVar(value='0')
+        check_flag = tk.StringVar(value=checkbox_value)
         ttk.Checkbutton(task_frame, variable=check_flag, font=custom_fonts.DEFAULT, command=change_entry_state)\
             .grid(row=0, column=0)
+        entry['state'] = NORMAL if check_flag.get() == '0' else DISABLED
 
         # create delete button
         ttk.Button(task_frame, text='DEL', font=custom_fonts.DEFAULT, command=lambda: task_frame.grid_forget())\
@@ -50,6 +53,11 @@ class Tasks(ttk.Frame):
 
         self.task_entries.append({'State': check_flag, 'Text': text})
 
-    # def set_tasks(self, tasks_frames):
-    #     for index, frame in enumerate(tasks_frames):
+    def set_tasks(self, tasks_to_add):
+        for task in tasks_to_add:
+            state = task['State']
+            text = task['Text']
+            self.add_new_task(state, text)
+
+
 
