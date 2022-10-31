b50ae5a881670009cd7d2d948afdf1a22801faa2
Filbert-code <alexlingfungfilbert@gmail.com>
2022-10-30 19:29:43 -0700

    Changed tasks input from Entry widget to Text widget. Tasks can now span multiple lines.

--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import tkinter as ttk
-from tkinter import N, S, E, W, DISABLED, NORMAL
+from tkinter import N, S, E, W, DISABLED, NORMAL, END
 from typing import List
+from tkinter.scrolledtext import ScrolledText
 
 import custom_fonts
 from task_data_entry import TaskDataEntry
@@ -16,8 +17,10 @@ class Tasks(ttk.Frame):
         self.tasks_database = tasks_database
 
         self.frame = ttk.Frame(self.parent)
+        self.tasks_frame = ttk.Frame(self.frame)
+        self.tasks_frame.grid(row=0, column=0)
 
-        self.next_task_row_num = 2
+        self.next_task_row_num = 0
 
         # tasks
         self.add_more_button = ttk.Button(self.frame, text='ADD', command=self.add_new_task, font=custom_fonts.DEFAULT)
@@ -29,13 +32,19 @@ class Tasks(ttk.Frame):
         #     self.set_tasks(tasks)
 
     def add_new_task(self, checkbox_value='0', text_value=''):
-        task_frame = ttk.Frame(self.frame)
-        task_frame.grid(row=self.next_task_row_num, column=0)
+        task_frame = ttk.Frame(self.tasks_frame)
+        task_frame.grid(row=self.next_task_row_num, column=0, pady=5)
         self.next_task_row_num += 1
+
+        def update_text_stringvar(event):
+            text.set(entry.get("1.0", "end -1 chars"))
+            print(text.get())
 
         # create entry box
         text = tk.StringVar(value=text_value)
-        entry = ttk.Entry(task_frame, textvariable=text, font=custom_fonts.DEFAULT, width=40)
+        entry = tk.Text(task_frame, font=custom_fonts.DEFAULT, width=40, height=2)
+        entry.insert(1.0, text.get())
+        entry.bind('<KeyRelease>', update_text_stringvar)
         entry.grid(row=0, column=1)
 
         def change_entry_state():
@@ -47,12 +56,12 @@ class Tasks(ttk.Frame):
         # completed task checkmark
         check_flag = tk.StringVar(value=checkbox_value)
         ttk.Checkbutton(task_frame, variable=check_flag, font=custom_fonts.DEFAULT, command=change_entry_state)\
-            .grid(row=0, column=0)
+            .grid(row=0, column=0, padx=5)
         entry['state'] = NORMAL if check_flag.get() == '0' else DISABLED
 
         # create delete button
         ttk.Button(task_frame, text='DEL', font=custom_fonts.DEFAULT, command=lambda: task_frame.grid_forget())\
-            .grid(row=0, column=2)
+            .grid(row=0, column=2, padx=5)
 
         # add the task to the database
         self.tasks_database.add_tasks(
